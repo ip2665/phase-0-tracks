859cd8e19fdcfc1a67d7c183de2c932df9b9031e
Kornelius <ip2665@gmail.com>
2016-11-07 13:33:54 -0800

    Add attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,10 @@
 class Santa
-    def initialize
+    def initialize(gender,ethnicity)
         puts "Initializing Santa instance..."
+        @gender = gender
+        @ethnicity = ethnicity
+        @reindeer_ranking = ["Rudolf","Dasher","Dancer","Prancer","Vixen","Comet","Cupid","Donner","Blitzen"]
+        @age = 0
     end 
 	def speak
 		puts "Ho, ho, ho! Haaaapy holidays!"
@@ -10,6 +14,7 @@ class Santa
 		puts "That was a good "+cookie_name+" !"
 	end 
 end 	
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 
-
-nick = Santa.new
+nick = Santa.new(example_genders[0],example_ethnicities[0])
