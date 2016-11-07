3f9366947cbe3beacde09ec30ffc9a805e58deb2
Kornelius <ip2665@gmail.com>
2016-11-07 13:49:51 -0800

    Add 3 attribute-changing methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -6,6 +6,7 @@ class Santa
         @reindeer_ranking = ["Rudolf","Dasher","Dancer","Prancer","Vixen","Comet","Cupid","Donner","Blitzen"]
         @age = 0
     end 
+	
 	def speak
 		puts "Ho, ho, ho! Haaaapy holidays!"
 	end 
@@ -13,8 +14,39 @@ class Santa
 	def eat_milk_and_cookies(cookie_name)
 		puts "That was a good "+cookie_name+" !"
 	end 
+    
+#Setter Methods    
+    def celebrate_birthday
+    	@age += 1
+    end 
+
+    def get_mad_at(name="")
+       	@reindeer_ranking.delete_if {|x| x == name}
+    	@reindeer_ranking << name
+    	@reindeer_ranking
+    end 
+
+    def gender(gender)
+    	@gender = gender
+    end 
+
+#Getter Methods
+    def age
+    	@age 
+    end     
+
+    def ethnicity
+    	@ethnicity
+    end 
+
 end 	
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 
 nick = Santa.new(example_genders[0],example_ethnicities[0])
+nick.get_mad_at("Vixen")
+
+nick.celebrate_birthday
+puts nick.age
+puts nick.ethnicity
+puts nick.get_mad_at("Vixen")
