533e696d2ff5128ede81aad86d8484c7959ec8e9
Kornelius <ip2665@gmail.com>
2016-11-08 13:31:19 -0800

    Add 4 instance methods to complete the challenge

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -7,4 +7,16 @@ class TodoList
 	def get_items
 		@chores_list
 	end 
+
+	def add_item(chore)
+		@chores_list << chore
+	end 
+
+	def delete_item(chore_item)
+		@chores_list.delete_if {|chore| chore == chore_item}
+	end 
+
+	def get_item(index)
+		@chores_list[index]
+	end 
 end 	
