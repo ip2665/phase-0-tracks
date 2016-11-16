2f01d9ce1f33756a238221e274b6e323a2940683
Kornelius <ip2665@gmail.com>
2016-11-16 10:21:30 -0800

    Add Object and build code to fill it up from array

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,9 +1,24 @@
 var colors = ["Blue","Black","White","Brown"]
 var names = ["Biscuit","Cookie","Chocolate","Caramel"]
 
+
+//driver code that add one element to each array
 console.log(colors);
 console.log(names);
 colors.push("Spotty");
 names.push("Candy");
 console.log(colors);
 console.log(names);
+
+
+//Object declaration
+var horses = {}
+
+
+//Code to build object elements
+for (i = 0; i < names.length ; i++ ) {
+		horses[names[i]] = colors[i];
+}
+
+//Display the object elements
+console.log(horses);
