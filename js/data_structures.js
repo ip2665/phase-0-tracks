eac4b72ed18ef6ce2bb9457eabc5a5bc6b141a72
Kornelius <ip2665@gmail.com>
2016-11-16 10:12:18 -0800

    Initialize and add two arrays

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ["Blue","Black","White","Brown"]
+var names = ["Biscuit","Cookie","Chocolate","Caramel"]
+
+console.log(colors);
+console.log(names);
+colors.push("Spotty");
+names.push("Candy");
+console.log(colors);
+console.log(names);
