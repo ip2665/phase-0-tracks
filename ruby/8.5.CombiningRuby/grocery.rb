3543653311071541bfa1eb189d189ed399f89b3d
Kornelius <ip2665@gmail.com>
2016-11-23 13:31:00 -0800

    Add Pseudocode

---
+++ ruby/8.5.CombiningRuby/grocery.rb
@@ -0,0 +1,26 @@
+#Grocery List
+require "sqlite3"
+
+#ADD  List
+	#Access list table and display current list
+	#Accept User inputs
+	#Add the input as new row
+
+#EDIT List
+	#Accept User inputs based on the list ID
+	#Accept User inputs for new values
+	#Update the list
+
+#DISPLAY Report
+	#Grocery list report
+
+def list_report
+end	
+	
+	#Store report
+def stores_report
+end
+
+#Driver Code
+
+
